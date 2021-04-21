60e101892037db224baa97d9e17e6aaa85fb2615
Johan Kladder <johankladder@gmail.com>
2021-04-21 11:40:23 +0200

    + Added stylings support

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,7 @@
 
     <div class="list-group">
         <a href="{{ route('stadia-plants.index') }}" class="list-group-item list-group-item-action">
-            Plants (0)
-        </a>
-        <a href="{{ route('stadia-plants.index') }}" class="list-group-item list-group-item-action">
-            Userdata
-        </a>
-        <a href="{{ route('stadia-plants.index') }}" class="list-group-item list-group-item-action">
-            Userdata
+            Plants ({{\JohanKladder\Stadia\Models\StadiaPlant::count()}})
         </a>
     </div>
 @endsection
